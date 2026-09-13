a5d93388a0cf9f9c5258db0beffb761444a6d910
treegus <cowtree28@gmail.com>
2026-09-14 01:47:38 +0900

    chore(#1) :: init spring boot project

---
+++ src/main/java/com/example/zukbambe/ZukbamBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.zukbambe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZukbamBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZukbamBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zukbambe/ZukbamBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zukbambe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZukbamBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
